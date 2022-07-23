147dabb169c487462a6f9a2ec38fd3b3ed346e56
yanazvdo <108288728+yanazvdo@users.noreply.github.com>
2022-07-23 18:41:09 -0300

    Add files via upload

---
+++ MeuArquivo.py
@@ -0,0 +1,22 @@
+nome = 'lira'
+print(type(nome))
+print(nome.capitalize())
+
+class TV():
+    #ATRIBUTOS:
+
+    cor = 'preta'
+    tamanho = 55
+    canal = 10
+    volume = 50
+
+    #MÉTODOS:
+
+    def mudar_canal(self):
+        pass
+
+    def mudar_volume(self):
+        pass
+
+    def ligar_desligar(self):
+        pass
