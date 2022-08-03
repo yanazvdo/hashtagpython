5e63900476a0cc78c82c232839209fefac5b195f
yanazvdo <108288728+yanazvdo@users.noreply.github.com>
2022-08-03 17:56:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,14 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
-from forms import FormLogin, FormCriarConta
 
-app = Flask(__name__)
+import Contas_bancos
+from Contas_bancos import ContaCorrente, CartaoCredito
+from Agencia import AgenciaComum, AgenciaPremium, AgenciaVirtual, Agencia
 
-lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
+#PROGRAMA
 
-app.config['SECRET_KEY'] = 'fb9f6e231bdcb1exit769c4d347bfb7cde49'
+# conta_lira = ContaCorrente('Lira', '000.000.000-00', 1234, 34062)
+# cartao_lira = CartaoCredito('Lira', conta_lira)
+# cartao_lira.senha = '123'
+# print(cartao_lira._senha)
+# print(conta_lira.__dict__)
 
-@app.route("/")
-def home():
-    return render_template('home.html')
-
-@app.route("/contato")
-def contato():
-    return render_template('contato.html')
-
-@app.route("/usuarios")
-def usuarios():
-    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form_login = FormLogin()
-    form_criarconta = FormCriarConta()
-
-    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        #fez login com sucesso:
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        #redirecionar para a homepage:
-        return redirect(url_for('home'))
-    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        #criou conta com sucesso
-        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
-        return redirect(url_for('home'))
-    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+agencia_premium_especial = AgenciaPremium(123123123, 90000000000)
